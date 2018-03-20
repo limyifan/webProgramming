32f3a0c60053feaf7b824395f0f8c5964a1b55e4
charles7020 <limyifan1997@gmail.com>
2018-03-20 23:23:06 +0000

    new

--- database.php
+++ database.php
@@ -1,18 +1,16 @@
 <?php
-
-$dsn = "mysql:host=localhost;dbname=webprogrammingcasem2";
-$username = "root";
-$password = "";
-
-try {
-    $db = new PDO($dsn, $username, $password);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    error_reporting(E_ALL);
-    // echo "database connected!!"; 
-} catch (PDOExceptionc $e) {
-    $error_message = $e->getMessage();
-    inclcude("database_error.php");
-    exit();
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'webprogrammingcasem2');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
