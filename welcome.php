774a1573a23c70d23e8ee06dffdbfe340496b2ed
charles7020 <limyifan1997@gmail.com>
2018-03-21 00:06:01 +0000

    new

--- welcome.php
+++ welcome.php
@@ -22,7 +22,9 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 <body>
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to our site.</h1>
-    </div>
+      
+    </div><br>
+       <img src="uploads/<?php echo htmlspecialchars($_SESSION['avatar']); ?>">
     <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
 </body>
 </html>
